821fc4b1ec86fd918e9e4587d4e1b104a0a61404
barton <todd3608pro@gmail.com>
2025-11-21 20:16:12 +0300

    initial commit

--- Binarytodecimal.py
+++ Binarytodecimal.py
@@ -1,11 +1,34 @@
-binary = input("Enter a binary number: ")
+print("Choose an option:")
+print("1. Binary to Decimal")
+print("2. Decimal to Binary")
 
-decimal = 0
-power = 0
+choice = input("Enter 1 or 2: ")
 
-for digit in reversed(binary):
-    if digit == '1':
-        decimal += 2 ** power
-    power += 1
+if choice == '1':
+    binary = input("Enter a binary number: ")
+    decimal = 0
+    power = 0
+    for digit in reversed(binary):
+        if digit not in ('0', '1'):
+            print("Invalid binary number!")
+            exit()
+        if digit == '1':
+            decimal += 2 ** power
+        power += 1
+    print("Decimal value =", decimal)
 
-print("Decimal value =", decimal)
+elif choice == '2':
+    decimal = int(input("Enter a decimal number: "))
+    if decimal < 0:
+        print("Please enter a non-negative number!")
+        exit()
+    binary = ''
+    if decimal == 0:
+        binary = '0'
+    while decimal > 0:
+        binary = str(decimal % 2) + binary
+        decimal = decimal // 2
+    print("Binary value =", binary)
+
+else:
+    print("Invalid choice!")
